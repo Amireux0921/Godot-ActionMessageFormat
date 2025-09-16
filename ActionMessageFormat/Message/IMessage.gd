6fb49e491a54bf71e64f42f4cf91a57515175b65
微光绘梦工作室 <102020235+Amireux0921@users.noreply.github.com>
2025-09-16 13:28:50 +0800

    Add files via upload

---
+++ ActionMessageFormat/Message/IMessage.gd
@@ -0,0 +1,3 @@
+##所有消息格式的基类[hr]
+##无论是内置的消息，还是自定义的消息，都必须要继承此类
+@abstract class_name IMessage extends ActionMessageFormat
---
+++ ActionMessageFormat/Types/IAmfObject.gd
@@ -0,0 +1,2 @@
+##所有继承他的子类必须实现ToObject()方法进行数据还原
+class_name IAmfObject extends ActionMessageFormat
